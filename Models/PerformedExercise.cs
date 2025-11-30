a67cc0e2c09808e96e386efd9aff4fc8d4048964
michalnowaczyk <pzx100590@student.wsb.poznan.pl>
2025-11-30 22:02:42 +0100

    Rename Models/erformedExercise.cs -> Models/PerformedExercise.cs and add .gitignore

--- Models/PerformedExercise.cs
+++ Models/PerformedExercise.cs
@@ -8,21 +8,31 @@ namespace BeFit.Models
 
         // Klucze obce
         [Required]
+        [Display(Name = "Sesja treningowa", Description = "Wybierz sesję treningową (data)")]
         public int TrainingSessionId { get; set; }
         public TrainingSession? TrainingSession { get; set; }
 
         [Required]
+        [Display(Name = "Typ ćwiczenia", Description = "Wybierz rodzaj ćwiczenia")]
         public int ExerciseTypeId { get; set; }
         public ExerciseType? ExerciseType { get; set; }
 
+        // Powiązanie z użytkownikiem
+        [Display(Name = "Użytkownik")]
+        public string? UserId { get; set; }
+        public ApplicationUser? User { get; set; }
+
         // Parametry wykonania
         [Range(0, double.MaxValue)]
+        [Display(Name = "Obciążenie (kg)", Description = "Obciążenie w kilogramach")]
         public double LoadKg { get; set; } // obciążenie w kg
 
         [Range(1, 1000)]
+        [Display(Name = "Liczba serii", Description = "Ile serii wykonano")]
         public int Sets { get; set; } // liczba serii
 
         [Range(1, 1000)]
+        [Display(Name = "Powtórzenia w serii", Description = "Liczba powtórzeń w pojedynczej serii")]
         public int RepsPerSet { get; set; } // liczba powtórzeń w serii
     }
 }
